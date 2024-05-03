9dde4cd5d804197130061430c14424e031b41680
Pavlo Korobchuk <pavlokorobchuk@pavlos-mbp.home>
2024-05-03 14:27:06 +0200

    login page added

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,5 +1,57 @@
-import React from "react";
+import { useFormik } from "formik";
+import * as yup from "yup";
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+
+import { registerFormValidationSchema } from "../utils";
+
+import { WithThemeProviderLayout } from "../layouts";
 
 export function Login() {
-  return <div>Login</div>;
+  const formik = useFormik({
+    initialValues: {
+      email: "foobar@example.com",
+      password: "foobar",
+    },
+    validationSchema: registerFormValidationSchema,
+    onSubmit: (values) => {
+      alert(JSON.stringify(values, null, 2));
+    },
+  });
+
+  return (
+    <WithThemeProviderLayout>
+      <>
+        <h1>Register</h1>
+        <div>
+          <form onSubmit={formik.handleSubmit}>
+            <TextField
+              fullWidth
+              id="email"
+              name="email"
+              label="Email"
+              value={formik.values.email}
+              onChange={formik.handleChange}
+              error={formik.touched.email && Boolean(formik.errors.email)}
+              helperText={formik.touched.email && formik.errors.email}
+            />
+            <TextField
+              fullWidth
+              id="password"
+              name="password"
+              label="Password"
+              type="password"
+              value={formik.values.password}
+              onChange={formik.handleChange}
+              error={formik.touched.password && Boolean(formik.errors.password)}
+              helperText={formik.touched.password && formik.errors.password}
+            />
+            <Button color="primary" variant="contained" fullWidth type="submit">
+              Submit
+            </Button>
+          </form>
+        </div>
+      </>
+    </WithThemeProviderLayout>
+  );
 }
